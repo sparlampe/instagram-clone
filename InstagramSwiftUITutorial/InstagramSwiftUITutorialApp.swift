fa43c9b6cca0dc2936cf357f332bfe083418cf4f
Stephan Dowless <dowless.stephan@gmail.com>
2020-12-26 11:51:39 -0500

    Initial Commit

---
+++ InstagramSwiftUITutorial/InstagramSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUITutorialApp.swift
+//  InstagramSwiftUITutorial
+//
+//  Created by Stephen Dowless on 12/26/20.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
